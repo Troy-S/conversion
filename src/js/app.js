02019e5b62af8180ea20637743871fc13c3c6e21
Troy-S <troysheed@gmail.com>
2021-06-02 12:47:15 +0100

    Fixed some npm script issues
    Renamed image (more concise name).
    Fixed npm scripts.
    Removed docs script as JsDoc not being used.

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@
 import timer from './module/timer';
 
 // Had to use this image with the text in the background as I wasn't supplied with the correct soundtrack-bold font (the one in the directory is not correct).
-import discount from '../assets/images/newsletter-background-with-text.png';
+import discount from '../assets/images/discount.png';
 
 const discountImage = document.getElementById('discount');
 discountImage.src = discount;
--- webpack/config.prod.babel.js
+++ webpack/config.prod.babel.js
@@ -40,6 +40,16 @@ module.exports = {
                 use: ['babel-loader']
             },
             {
+                test: /\.(png|jp(e*)g|svg|woff|woff2|eot|ttf|otf)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            },
+            {
                 test: /\.(css|scss)$/,
                 exclude: /node_modules/,
                 use: ExtractTextPlugin.extract({

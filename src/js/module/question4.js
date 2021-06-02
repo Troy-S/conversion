d95ef32e3a192c10d7e709b0220f0917a779b607
Troy-S <troysheed@gmail.com>
2021-06-02 10:20:06 +0100

    Conversion - Troy Sheed

--- src/js/module/question4.js
+++
@@ -1,6 +0,0 @@
-const products = document.getElementsByClassName('sale_discount__BlSRb');
-const secondProduct = products[1];
-const price = secondProduct.innerText.substring(1);
-const parsed = parseFloat(price);
-
-const totalValue = parsed / (parsed / 100);
